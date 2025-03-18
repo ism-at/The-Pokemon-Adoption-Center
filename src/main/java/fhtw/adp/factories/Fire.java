a267558385b8ded6af03b439d4efc314eae1273f
ismail <alagele.ismail@gmail.com>
2025-03-19 00:02:44 +0100

    first commit

---
+++ src/main/java/fhtw/adp/factories/Fire.java
@@ -0,0 +1,9 @@
+package fhtw.adp.factory;
+
+import fhtw.adp.PokemonAbstract;
+
+public class Fire extends PokemonAbstract {
+    public Fire(String name) {
+        super(name, "Fire");
+    }
+}
---
+++ src/main/java/fhtw/adp/factories/Ghost.java
@@ -0,0 +1,9 @@
+package fhtw.adp.factory;
+
+import fhtw.adp.PokemonAbstract;
+
+public class Ghost extends PokemonAbstract {
+    public Ghost(String name) {
+        super(name, "Ghost");
+    }
+}
